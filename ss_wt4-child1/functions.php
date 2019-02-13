0514fe418cdc110bd620f94839f5d11ab6159f05
rendy44 <deQueen44>
2019-02-13 10:37:20 +0700

    Initial commit

---
+++ ss_wt4-child1/functions.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 2/13/2019
+ * Time: 10:24 AM
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require get_stylesheet_directory() . '/inc/class-sswt5.php';
+$ssTestimonialsV2 = SSWT5::Instance();
+
+//$ssWT4template = new SSWT4_Template( get_template_directory() . '/templates' );
---
+++ ss_wt4-child1/inc/class-sswt5.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 2/13/2019
+ * Time: 10:19 AM
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'SSWT5' ) ) {
+	class SSWT5 {
+		public static function Instance() {
+			static $instance = null;
+			if ( $instance === null ) {
+				$instance = new SSWT5();
+			}
+
+			return $instance;
+		}
+
+		private function __construct() {
+			$this->load_front_end_assets();
+		}
+
+		function load_front_end_assets() {
+			require get_stylesheet_directory() . '/inc/class-sswt5-front-assets.php';
+			new SSWT5_Front_Assets();
+		}
+	}
+}
