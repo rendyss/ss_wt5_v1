04a6f788752226f5ff1774d08e57aed89b97ceed
rendy44 <deQueen44>
2019-02-14 10:31:47 +0700

    Change options field structure

--- ss_wt4-child1/options.php
+++ ss_wt4-child1/options.php
@@ -3,14 +3,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-return array(
-	array(
-		'name' => __( 'Global' ),
-		'type' => 'heading'
-	),
-	array(
-		'name' => __( 'Logo' ),
-		'id'   => 'ss_logo',
-		'type' => 'upload'
-	)
+$option[] = array(
+	'name' => __( 'Global' ),
+	'type' => 'heading'
 );
+
+$option[] = array(
+	'name' => __( 'Logo' ),
+	'id'   => 'ss_logo',
+	'type' => 'upload'
+);
+
+return $option;
